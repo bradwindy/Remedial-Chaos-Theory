502160690a0d9f19971f71f3d2db0e1dc2cc0713
Bradley Windybank <bradley.windybank@trademe.co.nz>
2022-08-05 11:05:51 +1200

    Initial Commit

---
+++ Remedial Chaos Theory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remedial Chaos Theory
+//
+//  Created by Bradley Windybank on 5/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Remedial Chaos Theory/Remedial_Chaos_TheoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remedial_Chaos_TheoryApp.swift
+//  Remedial Chaos Theory
+//
+//  Created by Bradley Windybank on 5/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Remedial_Chaos_TheoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
